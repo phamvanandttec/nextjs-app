4e4b6a3892189ea7909a0467f59ed98eff70ff5a
pvadttec <phamvanan@dttec.edu.vn>
2025-04-12 19:46:40 +0700

    fix: bug build

--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,12 @@ import Contact from "./components/contact/contact";
 import styles from "./page.module.css";
 
 export default async function Home() {
-  // Example: Sending data to /api/users from a form
-const res = await fetch('http://localhost:3000/api/user', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({ name: 'John Doe', email: 'john@example.com' }),
-});
-  console.log("RRR res", res);
-  
   return (
-     <main className={styles.mainContent}>
-        <Banner />
-        <Menu />
-        <About />
-        <Contact />
-      </main>
+    <main className={styles.mainContent}>
+      <Banner />
+      <Menu />
+      <About />
+      <Contact />
+    </main>
   );
 }
